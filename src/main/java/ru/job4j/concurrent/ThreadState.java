656ab384dc6b3164f1ab8e5dad87d0d59022fbfb
kavalerov25 <paradise_777@mail.ru>
2022-06-29 11:31:17 +0300

    1.1. Состояние нити. [#229175 #316359] FIX

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,27 +1,42 @@
 package ru.job4j.concurrent;
 
+/**
+ * Class ThreadState - Состояние потоков.
+ * 1. Threads. 1.1. Состояние нити.[#283070].
+ *
+ * @author Kirill Kavalerov (kavalerov24@gmail.com)
+ * @version 2
+ * @since 29.06.2022
+ */
+
 public class ThreadState {
+    /**
+     * Нить main должна дождаться завершения двух созданных нитей и вывести на консоль сообщение, что работа завершена.
+     */
     public static void main(String[] args) {
 
-        Thread first = new Thread(
-                () -> { }
+        var first = new Thread(
+                () -> {
+                    System.out.println("Creating first thread: " + Thread.currentThread().getName());
+                }
         );
 
-        Thread second = new Thread(
-                () -> { }
+        var second = new Thread(
+                () -> {
+                    System.out.println("Creating second thread: " + Thread.currentThread().getName());
+                }
         );
-        System.out.println(first.getState() + " " + "New first thread");
-        System.out.println(second.getState() + " " + "New second thread");
+
         first.start();
         second.start();
+        System.out.println(first.getState() + " " + " first thread");
+        System.out.println(second.getState() + " " + " second thread");
 
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState() + " " + "first");
-        }
-
-        while (second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState() + " " + "second");
         }
+
         System.out.println("The work is done");
     }
 }
