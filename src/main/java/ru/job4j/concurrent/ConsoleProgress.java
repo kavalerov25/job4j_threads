e249011e7bf911cd3a032b40568a8be0d12c6983
kavalerov25 <paradise_777@mail.ru>
2022-06-30 23:50:10 +0300

    3. Прерывание нити [#1019 #317099] fix

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,36 +1,40 @@
 package ru.job4j.concurrent;
 
+import static java.lang.Thread.*;
+
 /**
  * Class ConsoleProgress - Прерывание потоков.
  * 1. Threads. 3. Прерывание нити[#283074].
  *
  * @author Kirill Kavalerov (kavalerov24@gmail.com)
- * @version 1
- * @since 29.06.2022
+ * @version 1.1
+ * @since 30.06.2022
  */
 public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(10000);
+        sleep(10000);
         progress.interrupt();
     }
 
     @Override
     public void run() {
         int count = 0;
-        while (!Thread.currentThread().isInterrupted()) {
-            if (count == 4) {
+
+        while (!currentThread().isInterrupted()) {
+            char[] chars = new char[]{'-', '\\', '|', '/'};
+            System.out.print("\r load: " + chars[count]);
+            count++;
+
+            if (count == chars.length) {
                 count = 0;
             }
-            char[] chars = new char[]{'-', '\\', '|', '/'};
-            System.out.print("\r loading: " + chars[count]);
             try {
-                Thread.sleep(500);
+                sleep(500);
             } catch (InterruptedException e) {
                 break;
             }
-            count++;
         }
     }
 }
