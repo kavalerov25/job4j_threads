d0cb65bf111be191adaa8c559a5939ddc2f786c5
kavalerov25 <paradise_777@mail.ru>
2022-07-17 20:20:37 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int current;
+
+        do {
+            current = get();
+        } while (!count.compareAndSet(current, current + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
