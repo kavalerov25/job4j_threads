afe98eb6d44265fdc05cc8541df8a9e5b3f9f38a
kavalerov25 <paradise_777@mail.ru>
2022-07-20 23:00:43 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,28 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification implements AutoCloseable {
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+    pool.submit(() -> {
+        String subject = String.format("Notification %s to email %s.",
+                user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
+        send(subject, body, user.getEmail());
+    });
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        pool.shutdown();
+    }
+}
