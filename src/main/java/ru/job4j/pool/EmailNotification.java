3597c112f786569bfef07d0d1e72d4419a489590
kavalerov25 <paradise_777@mail.ru>
2022-07-24 20:26:48 +0300

    2. ExecutorService рассылка почты. [#63097 #324325] fix

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification implements AutoCloseable {
-    private ExecutorService pool = Executors.newFixedThreadPool(
+    private final ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors()
     );
 
